085a7b26fea37a15e5c81c9568e46bf038db9160
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-04-06 10:45:24 +0000

    acabao

---
+++ src/js/component/card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = ({imageUrl,name}) => {
+    return (
+        <div className="card stCard" >
+        <img src={imageUrl} className="card-img-top" alt="..."/>
+        <div className="card-body d-flex flex-column align-items-center">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    );
+};
+
+export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import NavBar from "./navBar";
+import Jumbotron from "./jumbotron";
+import Card from "./card";
 
-//create your first component
 const Home = () => {
+	const images = [
+		{ url: "https://t1.ea.ltmcdn.com/es/posts/7/4/3/como_ayudar_a_un_gatito_a_defecar_20347_600.jpg", name: "Gatito llorón" },
+		{ url: "https://t1.ea.ltmcdn.com/es/posts/1/5/1/los_10_gatos_mas_raros_del_mundo_22151_600.jpg", name: "Sacando lengua" },
+		{ url: "https://t2.uc.ltmcdn.com/es/posts/5/0/6/como_saber_si_un_gato_es_macho_o_hembra_con_fotos_10605_600_square.jpg", name: "Gatito tapado" },
+		{ url: "https://theavh.org/wp-content/uploads/scaredy-cat.jpg", name: "Gatito tricolor" }
+	]
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div >
+			<NavBar />
+			<div className="container">
+				<Jumbotron />
+				<div className="row">
+					{images.map((image, index) => (
+						<div key={index} className="col-md-3 col-sm-6">
+							<Card imageUrl={image.url} name={image.name} />
+						</div>
+					))}
+				</div>
+			</div>
 		</div>
 	);
 };
